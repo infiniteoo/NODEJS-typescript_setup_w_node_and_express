206f0f591034de6172f057b411e31593667ca05d
Troy Dorman <troydorman@gmail.com>
2021-08-12 19:35:17 -0600

    created a basic add function in typescript and logged it when the / route is hit

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,10 @@ import express, { Application, Request, Response, NextFunction } from "express";
 
 const app: Application = express();
 
+const add = (a: number, b: number): number => a + b;
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    console.log(add(1, 2));
   res.send("Hello World!");
 });
 
